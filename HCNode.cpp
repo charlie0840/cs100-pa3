c8af68003c8031e49bee0c33bfda9c7c592b12e5
charlie0840 <charlie0840@gmail.com>
2015-12-04 12:50:56 -0800

    Update HCNode.cpp

--- HCNode.cpp
+++ HCNode.cpp
@@ -1,15 +1,4 @@
-/*****************************************************************************
-  								Yiming Yu
-  								cs100feu
- 
-  			         Assignment 3
- File name:	HCNode.cpp
- Description:	nodes, each holding pointers to the left and right children of 
-		that node as well as a pointer to the parent node. What's new 
-		in the nodes of your Huffman tree are the members count and 
-		symbol, which hold the frequency of a particular symbol and 
-		its symbol ID, respectively. 
-*****************************************************************************/
+
 #include "HCNode.hpp"
 
 bool HCNode::operator<(const HCNode& other)
